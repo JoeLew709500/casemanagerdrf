ade78016b2eb33b9ef7a201e4019a430a6098869
Joe Lewis <walesrugby709500@gmail.com>
2024-05-23 20:44:44 +0100

    add authusers for authentication

---
+++ authusers/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import UserSerializer
+
+
+# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+
+class UserListView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = User.objects.all()
